8cf173c1cbf87d8f897128b8f71d94eedb405b42
skylar-mo <skylar.mogari@gmail.com>
2024-02-18 16:16:43 -0500

    fixed about.js folder

--- front-end/src/About.js
+++ front-end/src/About.js
@@ -1,24 +1,20 @@
-import { Link } from 'react-router-dom'
-import './About.css'
+function AboutUs() {
+  const apiUrl = "http://localhost:5002/about_us";
 
-/**
- * A React component that represents the Home page of the app.
- * @param {*} param0 an object holding any props passed to this component from its parent component
- * @returns The contents of this component, in JSX form.
- */
-const About = props => {
+  function fetchData() {
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(dataPhoto => {
+        document.getElementById("profile_pic").src = dataPhoto.image;
+        document.getElementById("student-description").innerText = dataPhoto.content;
+      });
+  }
   return (
-    <>
-      <h1>About Us</h1>
-      <p>
-      My name is Skylar Mogari, and I am a senior at NYU studying computer science with a minor in Astronomy. I am originally from Orange County, California. At NYU I am involved in a few clubs/organizations including TEDxNYU where I serve as the Director of Productions (and emcee). I am also involved with Girls Who Code(GWC), and the Persian Cultural Society(PCS). I have truly enjoyed living and exploring NYC for the past few years. My favorite activities here are going to museums, trying new restaurants and cafes, going for runs and walks, and much more. I plan on staying here after graduation to work as a software engineer. Ultimately, I hope to work in the aerospace industry where I can combine my passion for astronomy with my technical and administrative skills. 
-      </p>
-      <p>
-        Check out the <Link to="/messages">messages page</Link>.
-      </p>
-    </>
-  )
+    <div className="student-profile">
+        <img id="profile-pic" alt="Student Profile" className="profile-pic" />
+        <p id="student-description" className="student-description"></p>
+    </div>
+  );
 }
 
-// make this component available to be imported into any other file
-export default About
+export default AboutUs
